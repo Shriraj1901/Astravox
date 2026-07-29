9bfe48e7ea43eb1a9d1d2fe96da2e4d3c06fc6e1
shreeraj <shreerajjadhav19@gmail.com>
2026-07-29 17:01:42 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -26,3 +26,10 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+});
