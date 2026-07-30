50b652a743d3a164a03d1c9f5173fc67bcdfb2ed
shreeraj <shreerajjadhav19@gmail.com>
2026-07-30 10:06:47 +0530

    fix catch-all route for express 5 compatibility

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get('*', (req, res) => {
+app.get('/*splat', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
 });
 
